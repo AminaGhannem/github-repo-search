abcfd94e00afd173010be4444d1a2411ac111963
Amina Ghannem <amynaghannem@gmail.com>
2025-10-19 12:37:54 +0100

    fix: waiting for server

--- server.ts
+++ server.ts
@@ -17,6 +17,6 @@ app.use((req, res, next) => {
 // Serve built files
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${port}`);
 });
